84c93b613ecd9fc44cb08edb9d4414d18cffc026
Luwey-Silva <99865326+Luwey-Silva@users.noreply.github.com>
2022-08-08 14:20:44 +0200

    Add files via upload

---
+++ additional/scripts/commandHandler.js
@@ -0,0 +1,60 @@
+document.body.onkeydown = parseCommand;
+let blink = false; let command = "";
+setInterval(toggleCaret, 500);
+
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
+
+
+function toggleCaret() {
+    let caret = document.getElementById("commandCaret");
+    if (blink) {
+        blink = false; caret.style.color = "#00bd0d";
+    } else {
+        blink = true; caret.style.color = "#000000"; 
+    }
+}
+
+function executeCommand() {
+    switch (command.toLowerCase()) {
+        case "github":
+            return window.open("https://github.com/feschenko");
+        case "twitter":
+            return window.open("https://twitter.com/kiryssha");
+        case "telegram":
+            return window.open("https://t.me/kirillfeschenko");
+        case "habr":
+            return window.open("https://habr.com/en/users/0susboy0/");
+        case "linkedin":
+            return window.open("https://linkedin.com/in/kirillfeschenko");
+        case "email":
+            return window.open("mailto:swipduces@yandex.com");
+    }
+
+}
+
+function parseCommand(enteredKey) {
+    let dontPrint = ['Control', 'Alt', 'Shift', 'CapsLock', 'Tab', 'F1', 
+    'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10',
+    'F11', 'F12', 'F13', 'BrowserHome', 'ContextMenu',
+    'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown',
+    'Insert', 'Home', 'PageUp', 'Delete', 'End', 'PageDown',
+    'ScrollLock', 'Pause', 'Escape', 'OS', 'Meta',
+    '-', '=', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    let commandInput = document.getElementById("commandInput");
+    if (!dontPrint.includes(enteredKey.key)) {
+        if (enteredKey.key === "Backspace") {
+            command = command.substring(0, command.length - 1);
+        } else {
+            if (enteredKey.key === "Enter") {
+                executeCommand(); command = "";
+            } else {
+                if (command.length < 15) {
+                    command += enteredKey.key;
+                }
+            }
+        }
+        commandInput.innerText = command;
+    }
+}
+
